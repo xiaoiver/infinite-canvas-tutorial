e0df6f4a34603962e65a0a5b015ec4b6ca8ab4d1
xiaoiver <yuqi@youmind.ai>
2024-08-07 09:51:42 +0800

    chore: use pnpm to install playwright instead of npm

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ export default defineConfig({
   root: path.resolve('./__tests__/e2e/fixtures'),
   server: { port: 8080, open: '/' },
   base: '/infinitecanvas/',
-  define: {
-    global: {},
-  },
 });
