23acc1f1bb0240c19890acb116f3ade847527520
xiaoiver <pyqiverson@gmail.com>
2025-12-20 16:56:02 +0800

    fix: try to convert image returned from fal

--- packages/plugin-fal-ai/src/system.ts
+++ packages/plugin-fal-ai/src/system.ts
@@ -40,18 +40,13 @@ export class FalAISystem extends System {
         });
 
         // Convert Image to HTMLCanvasElement
-        const image = new Image();
-        image.src = result.data.image;
-        return new Promise((resolve) => {
-          image.onload = () => {
-            const canvas = document.createElement('canvas');
-            canvas.width = image.width;
-            canvas.height = image.height;
-            const ctx = canvas.getContext('2d');
-            ctx?.drawImage(image, 0, 0);
-            resolve({ image: canvas });
-          };
-        });
+        const image = result.data.image as unknown as HTMLImageElement;
+        const canvas = document.createElement('canvas');
+        canvas.width = image.width;
+        canvas.height = image.height;
+        const ctx = canvas.getContext('2d')!;
+        ctx.drawImage(image, 0, 0);
+        return { image: canvas };
       };
 
       // Do nothing here
