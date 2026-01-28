6558b480bface609f736cfb5360c9088c1a55329
xiaoiver <pyqiverson@gmail.com>
2026-01-28 18:11:22 +0800

    fix: tofixed(2) only apply to float number

--- packages/ecs/src/utils/serialize/points.ts
+++ packages/ecs/src/utils/serialize/points.ts
@@ -1,9 +1,19 @@
 import { BrushPoint } from '../../components/geometry/Brush';
 
+export function formatNumber(n: number) {
+  if (Number.isInteger(n)) {
+    return n.toString();
+  }
+  // 对有小数的值保留两位小数
+  return n.toFixed(2);
+}
+
 export function serializePoints(points: [number, number][]) {
-  return points.map(([x, y]) => `${x.toFixed(2)},${y.toFixed(2)}`).join(' ');
+  return points.map(([x, y]) => `${formatNumber(x)},${formatNumber(y)}`).join(' ');
 }
 
 export function serializeBrushPoints(points: BrushPoint[]) {
-  return points.map(({ x, y, radius }) => `${x.toFixed(2)},${y.toFixed(2)},${radius.toFixed(2)}`).join(' ');
+  return points
+    .map(({ x, y, radius }) => `${formatNumber(x)},${formatNumber(y)},${formatNumber(radius)}`)
+    .join(' ');
 }
